5f95a01a5022cf6b6a41c2339b536c1e973b005a
Fiona Y <fyeung21@gmail.com>
2022-04-14 03:33:40 -0700

    Add jQuery document ready function

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,6 +1,12 @@
 'use strict';
 
-$('#hamburger-btn').on('click', event => {
-    console.log('active');
-    $('.mobile-menu').toggleClass('mobile-menu-active');
+// jQuery document ready
+$(() => {
+
+    // hamburger menu function for mobile
+    $('#hamburger-btn').on('click', event => {
+        console.log('active');
+        $('.mobile-menu').toggleClass('mobile-menu-active');
+    });
+    
 });
